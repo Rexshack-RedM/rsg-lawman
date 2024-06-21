5f74600057753beb7af5060d73fa9c5d4db6fb41
RexshackGaming <rexshackuk@gmail.com>
2024-06-21 15:34:59 +0100

    law alert to only law enforcement officers

--- client/client.lua
+++ client/client.lua
@@ -104,70 +104,73 @@ end)
 -- lawman alert
 ------------------------------------------
 RegisterNetEvent('rsg-lawman:client:lawmanAlert', function(coords, text)
+    RSGCore.Functions.GetPlayerData(function(PlayerData)
+        if PlayerData.job.type == "leo" then
+            local blip = BlipAddForCoords(joaat('BLIP_STYLE_CREATOR_DEFAULT'), coords.x, coords.y, coords.z)
+            local blip2 = BlipAddForCoords(joaat('BLIP_STYLE_COP_PERSISTENT'), coords.x, coords.y, coords.z)
+            local blipText = Lang:t('lang12')..'- %{text}', {value = text}
+            SetBlipSprite(blip, joaat('blip_ambient_law'))
+            SetBlipSprite(blip2, joaat('blip_overlay_ring'))
+            BlipAddModifier(blip, joaat('BLIP_MODIFIER_AREA_PULSE'))
+            BlipAddModifier(blip2, joaat('BLIP_MODIFIER_AREA_PULSE'))
+            SetBlipScale(blip, 0.8)
+            SetBlipScale(blip2, 2.0)
+            SetBlipName(blip, text)
+            SetBlipName(blip2, text)
 
-    local blip = BlipAddForCoords(joaat('BLIP_STYLE_CREATOR_DEFAULT'), coords.x, coords.y, coords.z)
-    local blip2 = BlipAddForCoords(joaat('BLIP_STYLE_COP_PERSISTENT'), coords.x, coords.y, coords.z)
-    local blipText = Lang:t('lang12')..'- %{text}', {value = text}
-    SetBlipSprite(blip, joaat('blip_ambient_law'))
-    SetBlipSprite(blip2, joaat('blip_overlay_ring'))
-    BlipAddModifier(blip, joaat('BLIP_MODIFIER_AREA_PULSE'))
-    BlipAddModifier(blip2, joaat('BLIP_MODIFIER_AREA_PULSE'))
-    SetBlipScale(blip, 0.8)
-    SetBlipScale(blip2, 2.0)
-    SetBlipName(blip, text)
-    SetBlipName(blip2, text)
+            blipEntries[#blipEntries + 1] = {coords = coords, handle = blip}
+            blipEntries[#blipEntries + 1] = {coords = coords, handle = blip2}
 
-    blipEntries[#blipEntries + 1] = {coords = coords, handle = blip}
-    blipEntries[#blipEntries + 1] = {coords = coords, handle = blip2}
+            -- Add GPS Route
 
-    -- Add GPS Route
-
-    if Config.AddGPSRoute then
-        StartGpsMultiRoute(`COLOR_GREEN`, true, true)
-        AddPointToGpsMultiRoute(coords)
-        SetGpsMultiRouteRender(true)
-    end
-
-    -- send notifcation
-    lib.notify({ title = text, type = 'inform', duration = 7000 })
-
-    CreateThread(function ()
-        while timer ~= 0 do
-            Wait(180 * 4)
-
-            local pcoord = GetEntityCoords(cache.ped)
-            local distance = #(coords - pcoord)
-            timer = timer - 1
-
-            if Config.Debug then
-                print('Distance to Alert Blip: '..tostring(distance)..' metres')
+            if Config.AddGPSRoute then
+                StartGpsMultiRoute(`COLOR_GREEN`, true, true)
+                AddPointToGpsMultiRoute(coords)
+                SetGpsMultiRouteRender(true)
             end
 
-            if timer <= 0 or distance < 5.0 then
-                for i = 1, #blipEntries do
-                    local blips = blipEntries[i]
-                    local bcoords = blips.coords
+            -- send notifcation
+            lib.notify({ title = text, type = 'inform', duration = 7000 })
 
-                    if coords == bcoords then
-                        if Config.Debug then
-                            print('')
-                            print('Blip Coords: '..tostring(bcoords))
-                            print('Blip Removed: '..tostring(blipEntries[i].handle))
-                            print('')
+            CreateThread(function ()
+                while timer ~= 0 do
+                    Wait(180 * 4)
+
+                    local pcoord = GetEntityCoords(cache.ped)
+                    local distance = #(coords - pcoord)
+                    timer = timer - 1
+
+                    if Config.Debug then
+                        print('Distance to Alert Blip: '..tostring(distance)..' metres')
+                    end
+
+                    if timer <= 0 or distance < 5.0 then
+                        for i = 1, #blipEntries do
+                            local blips = blipEntries[i]
+                            local bcoords = blips.coords
+
+                            if coords == bcoords then
+                                if Config.Debug then
+                                    print('')
+                                    print('Blip Coords: '..tostring(bcoords))
+                                    print('Blip Removed: '..tostring(blipEntries[i].handle))
+                                    print('')
+                                end
+
+                                RemoveBlip(blipEntries[i].handle)
+                            end
                         end
 
-                        RemoveBlip(blipEntries[i].handle)
+                        timer = Config.AlertTimer
+
+                        if Config.AddGPSRoute then
+                            ClearGpsMultiRoute(coords)
+                        end
+
+                        return
                     end
                 end
-
-                timer = Config.AlertTimer
-
-                if Config.AddGPSRoute then
-                    ClearGpsMultiRoute(coords)
-                end
-
-                return
-            end
+            end)
         end
     end)
 end)
